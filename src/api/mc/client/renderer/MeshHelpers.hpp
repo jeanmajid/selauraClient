3120c062962302109c7200c383bed883064a5ee1
notchyves <chyvescontact@gmail.com>
2025-11-02 15:08:29 -0500

    funny blue minecraft

--- src/api/mc/client/renderer/MeshHelpers.hpp
+++ src/api/mc/client/renderer/MeshHelpers.hpp
@@ -1,15 +1,16 @@
 #pragma once
 #include <bit>
 #include <hooks/memory.hpp>
+#include <print>
 
 class ScreenContext;
 class Tessellator;
 namespace mce {
-    class MaterialPtr;
+    class MaterialPtr {};
 };
 
 namespace MeshHelpers {
-    inline void renderMeshImmediately(ScreenContext& screenContext, Tessellator& tessellator, const mce::MaterialPtr& material, char* a4[0x58] = {}) {
+    inline void renderMeshImmediately(ScreenContext* screenContext, Tessellator* tessellator, const mce::MaterialPtr* material, char* a4) {
         using fn_t = decltype(&MeshHelpers::renderMeshImmediately);
         static fn_t fn = std::bit_cast<fn_t>(selaura::find_signature<"E8 ? ? ? ? C6 46 ? ? F3 41 0F 10 5F">());
         return (*fn)(screenContext, tessellator, material, a4);
--- test/main.cpp
+++ test/main.cpp
@@ -15,9 +15,8 @@ BOOL APIENTRY DllMain(HINSTANCE, DWORD, LPVOID) {
 
 void after_ui(selaura::SetupAndRenderEvent<selaura::event_phase::post>& ev) {
     auto scrn = ev.mCtx->mScreenContext;
-    scrn->tessellator->begin(mce::PrimitiveMode::LineStrip, 1);
-    scrn->tessellator->color(1.f, 1.f, 1.f, 1.f);
-    scrn->tessellator->vertex(0, 0, 0);
+    scrn->tessellator->begin(mce::PrimitiveMode::QuadList, 8);
+    scrn->tessellator->color(0.f, 0.f, 1.f, 1.f);
 
     //ev.mCtx->mClientInstance->mGuiData->displayLocalMessage("this is from a plugin!");
 }
